5d84da0cfb38d9e94501195a062c49661aaed652
Gummyii <sunny.yuan0333@gmail.com>
2020-01-16 11:44:29 -0800

    This is my second time submitting this task

--- Test1.py
+++ Test1.py
@@ -14,14 +14,16 @@ deeppink = soy.atoms.Color('deeppink')
 darkslategray = soy.atoms.Color('darkslategray')
 red = soy.atoms.Color('red')
 green = soy.atoms.Color('green')
+brick = soy.textures.Texture('brick.jpg')
+gravel = soy.textures.Texture('gravel.jpg')
 
 material1 = soy.materials.Textured()
-material1.bumpmap = soy.textures.Bumpmap("media/grass.jpg")
-material1.colormap = soy.textures.Cubemap("checkered", [deeppink, blue, darkslategray], 1,2,3)
+#material1.colormap = soy.textures.Cubemap("checkered", [deeppink, blue, darkslategray], 1,2,3)
 
 
 room['cube1'] = soy.bodies.Box(soy.atoms.Position((1.0, 0.2, 1)))
 room['cube1'].material = material1
+room['cube1'].material = soy.materials.Textured(colormap = gravel)
 room['light1'] = soy.bodies.Light(soy.atoms.Position((1.3, 0.2, 1)))
 room['light1'].diffuse = soy.atoms.Color('deeppink')
 room['light1'].specular = soy.atoms.Color('deeppink')
@@ -33,14 +35,16 @@ material2 = soy.materials.Colored('yellow')
 #room['cube2'] = soy.bodies.Sphere(soy.atoms.Position((3.0, 0.2, 1)))
 
 material3 = soy.materials.Textured()
-material3.bumpmap = soy.textures.Bumpmap('media/lava.png')
-material3.colormap = soy.textures.Cubemap("checkered", [red, green], 1,2)
+#material3.bumpmap = soy.textures.Bumpmap('brick.jpg')
+#colormap = soy.materials.Textured(colormap = brick)
 
 room['cube2'] = soy.bodies.Box(soy.atoms.Position((3,0.2,1)))
 room['cube2'].material = material2
+room['cube2'].material = soy.materials.Colored('yellow')
 
 room['sphere1'] = soy.bodies.Sphere()
 room['sphere1'].material = material3
+room['sphere1'].material = soy.materials.Textured(colormap = brick)
 
 
 
